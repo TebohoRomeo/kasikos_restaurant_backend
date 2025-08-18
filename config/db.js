f990e03ec87e532d83b0fe7673376c20c353203d
NoiBuilds <lekhalontsoaki@gmal.com>
2025-08-18 15:48:44 +0200

    IPV4 not allowed to reach host

--- config/db.js
+++ config/db.js
@@ -1,22 +1,10 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
-
-// module.exports = pool;
-
-
-
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  host: 'db.xsiixlvjgwcdlwvnlotp.supabase.co', // explicitly set host
+  host: 'db.nrxjixidnqbgvehvxbxq.supabase.co', // explicitly set host
   port: 5432,                                   // explicitly set port
   // Add this line to force IPv4 lookup
   keepAlive: true,
@@ -36,10 +24,3 @@ module.exports = {
   pool,
   getClient
 };
-
-// import postgres from 'postgres'
-
-// const connectionString = process.env.DATABASE_URL
-// const sql = postgres(connectionString)
-
-// export default sql
