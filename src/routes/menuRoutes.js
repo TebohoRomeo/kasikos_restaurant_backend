842983eaa56cb4908a8bb7b76a2ed7c546fe2e61
TebohoRomeo <teboholekhalo13@gmail.com>
2025-12-10 10:51:25 +0200

    resolved the blocking bugs and remaining db connection

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 import upload from '../config/multer.js';
-import menuController from '../controllers/menuController.js';
+import { createMenuItem, listMenus } from '../controllers/menuController.js';
 import { requireAuth, requireRestaurantOwner } from '../middleware/authMiddleware.js';
 
-router.post('/', menuController.createMenuItem); // requireAuth, requireRestaurantOwner -- set let restrictions for piloting
-router.post('/:menuId/items', requireAuth, requireRestaurantOwner, upload.single('image'), menuController.createMenuItem);
-router.get('/restaurant/:restaurantId', menuController.listMenus);
-router.get('/:menuId/items', menuController.listMenus);
+router.post('/', createMenuItem); // requireAuth, requireRestaurantOwner -- set let restrictions for piloting
+router.post('/:menuId/items', requireAuth, requireRestaurantOwner, upload.single('image'), createMenuItem);
+router.get('/restaurant/:restaurantId', listMenus);
+router.get('/:menuId/items', listMenus);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import app from '../app.js';
 import dotenv from 'dotenv';
+import { pool } from '../src/config/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 (async (req, res) => {
   try {
-    const result = await Pool.query('SELECT NOW()');
+    const result = await pool.query('SELECT NOW()');
     console.log('DB Connected:', res.rows[0]);
     res.json({ time: result.rows[0].now });
   } catch (err) {
